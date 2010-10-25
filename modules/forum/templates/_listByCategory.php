1e74ddcb741d03f4f3ef29f06ae76f400fff95d0
4levels <info@4levels.org>
2010-10-25 19:29:44 +0200

    Updates to listers to comply with phpBB html

--- modules/forum/templates/_listByCategory.php
+++ modules/forum/templates/_listByCategory.php
@@ -1,8 +1,59 @@
 <?php // Vars: $forumPager
 
-echo $forumPager->renderNavigationTop();
+echo $forumPager->renderNavigationTop(); ?>
 
-echo _open('ul.elements');
+<?php #foreach ($boardPager as $board) { ?>
+
+  <div class="forabg">
+    <div class="inner"><span class="corners-top"><span></span></span>
+      <ul class="topiclist">
+        <li class="header">
+          <dl class="icon">
+            <dt><?php echo _link($dm_page) ?></dt>
+            <dd class="topics"><?php echo __('Topics') ?></dd>
+            <dd class="posts"><?php echo __('Posts') ?></dd>
+            <dd class="lastpost"><span><?php echo __('Last post') ?></span></dd>
+          </dl>
+        </li>
+      </ul>
+      <ul class="topiclist forums">
+        <?php foreach ($forumPager as $forum) { ?>
+        <li class="row">
+          <dl class="icon" style="background-image: url({forumrow.FORUM_FOLDER_IMG_SRC}); background-repeat: no-repeat;">
+            <dt title="<?php echo $forum ?>">
+            <!-- IF S_ENABLE_FEEDS and forumrow.S_FEED_ENABLED <a class="feed-icon-forum" title="{L_FEED} - {forumrow.FORUM_NAME}" href="{U_FEED}?f={forumrow.FORUM_ID}"><img src="{T_THEME_PATH}/images/feed.gif" alt="{L_FEED} - {forumrow.FORUM_NAME}" /></a> -->
+
+              <!--<span class="forum-image">{forumrow.FORUM_IMAGE}</span> -->
+              <?php echo _link($forum)->set('.forumtitle') ?><br />
+  					  <?php echo $forum->description ?>
+              <!-- <br /><strong>{forumrow.L_MODERATOR_STR}:</strong> {forumrow.MODERATORS} -->
+              <!-- IF forumrow.SUBFORUMS and forumrow.S_LIST_SUBFORUMS <br /><strong><?php echo __('Subforums') ?>:</strong> {forumrow.SUBFORUMS} ENDIF -->
+            </dt>
+            <!-- <dd class="redirect"><span>{L_REDIRECTS}: {forumrow.CLICKS}</span></dd> -->
+            <dd class="topics"><?php echo $forum->NbTopics ?> <dfn><?php echo __('Topics') ?></dfn></dd>
+            <dd class="posts"><?php echo $forum->NbPosts ?> <dfn><?php echo __('Posts') ?></dfn></dd>
+            <dd class="lastpost"><span>
+                <?php if ($forum->NbUnapprovedTopics) { ?><img src="/dmForumPlugin/images/icons/icon_topic_unapproved.gif" alt="<?php echo __('At least one topic is not apprived in this forum') ?>" /><?php } ?>
+                <?php if ($forum->NbPosts) { ?>
+                <!-- IF forumrow.LAST_POST_TIME --><dfn><?php echo __('Last post') ?></dfn> <?php echo __('by') ?> <?php echo _link($forum->LastPost->UpdatedBy->id ? $forum->LastPost->UpdatedBy : $forum->LastPost->CreatedBy) ?>
+                <!-- IF not S_IS_BOT --><?php echo _link($forum->LastPost->Topic) ?> <!-- ENDIF --><br /><?php echo format_datetime($forum->LastPost->updated_at) ?>
+                <?php } else { ?>
+                <?php echo __('No posts') ?><br />&nbsp;
+                <?php } ?>
+              </span>
+            </dd>
+          </dl>
+        </li>
+        <?php } ?>
+
+      </ul>
+
+      <span class="corners-bottom"><span></span></span></div>
+  </div>
+
+<?php #} ?>
+
+<?php echo _open('ul.elements');
 
 foreach ($forumPager as $forum)
 {
--- modules/topic/templates/_listByForum.php
+++ modules/topic/templates/_listByForum.php
@@ -1,8 +1,67 @@
+<h2><?php echo _link($dm_page) ?></h2>
+
+<!-- IF FORUM_DESC or MODERATORS or U_MCP -->
+<div>
+	<!-- NOTE: remove the style="display: none" when you want to have the forum description on the forum body -->
+	<!-- IF FORUM_DESC --><div style="display: none !important;">{FORUM_DESC}<br /></div><!-- ENDIF -->
+	<!-- IF MODERATORS --><p><strong><!-- IF S_SINGLE_MODERATOR -->{L_MODERATOR}<!-- ELSE -->{L_MODERATORS}<!-- ENDIF -->:</strong> {MODERATORS}</p><!-- ENDIF -->
+</div>
+<!-- ENDIF -->
+
 <?php // Vars: $topicPager
 
 echo $topicPager->renderNavigationTop();
 
-echo _open('ul.elements');
+?>
+
+	<!-- IF topicrow.S_FIRST_ROW or not topicrow.S_TOPIC_TYPE_SWITCH -->
+  <!-- IF topicrow.S_TOPIC_TYPE_SWITCH and (topicrow.S_POST_ANNOUNCE or topicrow.S_POST_GLOBAL) -- announcement<!-- ENDIF -->
+		<div class="forumbg">
+		<div class="inner"><span class="corners-top"><span></span></span>
+		<ul class="topiclist">
+			<li class="header">
+				<dl class="icon">
+					<dt><!-- IF S_DISPLAY_ACTIVE -->{L_ACTIVE_TOPICS}<!-- ELSEIF topicrow.S_TOPIC_TYPE_SWITCH and (topicrow.S_POST_ANNOUNCE or topicrow.S_POST_GLOBAL) -->{L_ANNOUNCEMENTS}<!-- ELSE -->{L_TOPICS}<!-- ENDIF --></dt>
+					<dd class="posts">{L_REPLIES}</dd>
+					<dd class="views">{L_VIEWS}</dd>
+					<dd class="lastpost"><span>{L_LAST_POST}</span></dd>
+				</dl>
+			</li>
+		</ul>
+		<ul class="topiclist topics">
+	<!-- ENDIF -->
+
+<?php
+$row_class = 'bg1';
+$topic_type = '';
+foreach ($topicPager as $topic)
+{
+  ?><!-- IF topicrow.S_POST_GLOBAL -- global-announce<!-- ENDIF --><!-- IF topicrow.S_POST_ANNOUNCE -- announce<!-- ENDIF --><!-- IF topicrow.S_POST_STICKY -- sticky<!-- ENDIF --><!-- IF topicrow.S_TOPIC_REPORTED -- reported<!-- ENDIF -->
+		<li class="row <?php echo $row_class ?>">
+			<dl class="icon" style="background-image: url({topicrow.TOPIC_FOLDER_IMG_SRC}); background-repeat: no-repeat;">
+				<dt style="background-image: url({T_ICONS_PATH}{topicrow.TOPIC_ICON_IMG}); background-repeat: no-repeat;" title="<?php echo $topic ?>">
+          <!-- IF topicrow.S_UNREAD_TOPIC --><a href="{topicrow.U_NEWEST_POST}">{NEWEST_POST_IMG}</a> <!-- ENDIF --><?php echo _link($topic)->set('.topictitle') ?>
+					<!-- IF topicrow.S_TOPIC_UNAPPROVED or topicrow.S_POSTS_UNAPPROVED --><a href="{topicrow.U_MCP_QUEUE}">{topicrow.UNAPPROVED_IMG}</a> <!-- ENDIF -->
+					<!-- IF topicrow.S_TOPIC_REPORTED --><a href="{topicrow.U_MCP_REPORT}">{REPORTED_IMG}</a><!-- ENDIF --><br />
+					<!-- IF topicrow.PAGINATION --><strong class="pagination"><span>{topicrow.PAGINATION}</span></strong><!-- ENDIF -->
+					<!-- IF topicrow.ATTACH_ICON_IMG -->{topicrow.ATTACH_ICON_IMG} <!-- ENDIF -->{L_POST_BY_AUTHOR} {topicrow.TOPIC_AUTHOR_FULL} &raquo; {topicrow.FIRST_POST_TIME}
+				</dt>
+				<dd class="posts"><?php $topic->Posts->count() ?> <dfn>{L_REPLIES}</dfn></dd>
+				<dd class="views">{topicrow.VIEWS} <dfn>{L_VIEWS}</dfn></dd>
+				<dd class="lastpost"><span><dfn>{L_LAST_POST} </dfn>{L_POST_BY_AUTHOR} {topicrow.LAST_POST_AUTHOR_FULL}
+					<!-- IF not S_IS_BOT --><a href="{topicrow.U_LAST_POST}">{LAST_POST_IMG}</a> <!-- ENDIF --><br />{topicrow.LAST_POST_TIME}</span>
+				</dd>
+			</dl>
+		</li>
+<?php } ?>
+	<!-- IF topicrow.S_LAST_ROW -->
+			</ul>
+		<span class="corners-bottom"><span></span></span></div>
+	</div>
+
+
+
+<?php echo _open('ul.elements');
 
 foreach ($topicPager as $topic)
 {
